5b7599006ebec008fb209e6d3dcff62bde0142fe
MattiaBologna <mattiabologna01@gmail.com>
2024-04-24 13:17:38 +0200

    Add: footer payments

--- src/components/sections/PaymentsSection.vue
+++ src/components/sections/PaymentsSection.vue
@@ -7,7 +7,18 @@ export default {
 
 <template>
     <section class="payment__section">
-        Pay
+        <div class="payment__top">
+            <p class="web__info"> &copy Copyright 2012 - 2020</p>
+            <p>|</p>
+            <p class="web__info"> Avada Theme by Theme Fusion</p>
+            <p>|</p>
+            <p class="web__info"> All Rights Reserved</p>
+            <p>|</p>
+            <p class="web__info"> Powered by WordPress</p>
+        </div>
+        <div class="payment__bottom">
+            <img src="/payment_cards_footer.png" alt="">
+        </div>
     </section>
 </template>
 
@@ -16,5 +27,19 @@ export default {
 <style lang="scss" scoped>
 .payment__section {
     background-color: #1D1F22;
+}
+
+.payment__top {
+    padding-top: 40px;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+}
+
+.payment__bottom {
+    display: flex;
+    justify-content: center;
+    padding-bottom: 20px;
 }
 </style>
